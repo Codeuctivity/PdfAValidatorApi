5d573e3062015955f821e1122e3fd1a779621477
Stefan Seeland <stefan.seeland@gmx.at>
2018-12-01 11:16:46 +0100

    updated nuget metadata, added test

--- PDfAValidatorTest/PdfAValidatorTest.cs
+++ PDfAValidatorTest/PdfAValidatorTest.cs
@@ -15,12 +15,34 @@ namespace PDfAValidatorTest
         }
 
         [Fact]
+        public void ShouldGetDetailedReportFromPdfA()
+        {
+            using (var pdfAValidator = new PdfAValidator.PdfAValidator())
+            {
+                var result = pdfAValidator.ValidateWithDetailedReport(@"./TestPdfFiles/FromLibreOffice.pdf");
+                Assert.True(result.jobs.job.validationReport.isCompliant);
+                Assert.True(result.jobs.job.validationReport.profileName == "PDF/A-1A validation profile");
+            }
+        }
+
+        [Fact]
         public void ShouldDetectNonCompliantPdfA()
         {
             using (var pdfAValidator = new PdfAValidator.PdfAValidator())
             {
                 var result = pdfAValidator.Validate(@"./TestPdfFiles/FromLibreOfficeNonPdfA.pdf");
                 Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public void ShouldGetDetailedReportFromNonCompliantPdfA()
+        {
+            using (var pdfAValidator = new PdfAValidator.PdfAValidator())
+            {
+                var result = pdfAValidator.ValidateWithDetailedReport(@"./TestPdfFiles/FromLibreOfficeNonPdfA.pdf");
+                Assert.False(result.jobs.job.validationReport.isCompliant);
+                Assert.True(result.jobs.job.validationReport.profileName == "PDF/A-1B validation profile");
             }
         }
     }
