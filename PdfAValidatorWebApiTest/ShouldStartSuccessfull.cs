a1f753462ff21816e32bfcbd5eb7dfce203c5910
Stefan Seeland <stefan.seeland@gmail.com>
2020-06-23 00:54:03 +0200

    Added integrative test

--- PdfAValidatorWebApiTest/ShouldStartSuccessfull.cs
+++ PdfAValidatorWebApiTest/ShouldStartSuccessfull.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -30,6 +32,28 @@ namespace CodeuctivityWebApiTest
             response.EnsureSuccessStatusCode();
             Assert.Equal(contentType,
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task ShouldValidatePdf()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), "http://localhost/api/PdfAValidator");
+            request.Headers.TryAddWithoutValidation("accept", "*/*");
+
+            var multipartContent = new MultipartFormDataContent();
+            var file1 = new ByteArrayContent(File.ReadAllBytes("../../../FromLibreOffice.pdf"));
+            file1.Headers.Add("Content-Type", "application/pdf");
+            multipartContent.Add(file1, "pdfFile", Path.GetFileName("FromLibreOffice.pdf"));
+            request.Content = multipartContent;
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
         }
     }
 }
