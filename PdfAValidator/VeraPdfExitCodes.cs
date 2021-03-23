aead1c7a4f6269c988bf80473c57de2bed0e995c
Stefan Seeland <168659+stesee@users.noreply.github.com>
2021-03-23 21:45:27 +0100

    Azure build pipeline config (#23)
    Enabled validation of on non PdfAs

---
+++ PdfAValidator/VeraPdfExitCodes.cs
@@ -0,0 +1,36 @@
+﻿namespace Codeuctivity
+{
+    /// <summary>
+    /// Vera PDF Exit codes that we handle
+    /// </summary>
+    public static class VeraPdfExitCodes
+    {
+        /// <summary>
+        /// All files valid
+        /// </summary>
+        public static int AllFilesValid { get; } = 0;
+
+        /// <summary>
+        /// Invalid PDF/A files found
+        /// </summary>
+        public static int InvalidPdfAFilesFound { get; } = 1;
+
+        /// <summary>
+        /// Failed to parse on or more files
+        /// </summary>
+        public static int FailedToParseOneOrMoreFiles { get; } = 7;
+
+        /// <summary>
+        /// Some PDFs encrypted
+        /// </summary>
+        public static int SomePdfsEncrypted { get; } = 8;
+
+        /// <summary>
+        /// Returns true if we can parse the output as xml
+        /// </summary>
+        public static bool CanExitCodeBeParsed(int exitCode)
+        {
+            return exitCode == AllFilesValid || exitCode == InvalidPdfAFilesFound || exitCode == FailedToParseOneOrMoreFiles || exitCode == SomePdfsEncrypted;
+        }
+    }
+}
--- PdfAValidatorTest/TestExecuteables/exitcode0.sh
+++ PdfAValidatorTest/TestExecuteables/exitcode0.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo "This is not XML"
 exit 0
